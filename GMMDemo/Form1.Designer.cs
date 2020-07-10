cba4e9a1c64b8c7145050dc3e5c11aa1b8b2d3f4
Haohan Lin <haohan.lin@outlook.com>
2020-07-10 14:34:25 -0700

    EM stability

--- GMMDemo/Form1.Designer.cs
+++ GMMDemo/Form1.Designer.cs
@@ -59,20 +59,24 @@
             // label_status
             // 
             this.label_status.AutoSize = true;
-            this.label_status.Location = new System.Drawing.Point(12, 530);
+            this.label_status.Location = new System.Drawing.Point(22, 856);
+            this.label_status.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label_status.Name = "label_status";
-            this.label_status.Size = new System.Drawing.Size(35, 13);
+            this.label_status.Size = new System.Drawing.Size(76, 21);
             this.label_status.TabIndex = 0;
             this.label_status.Text = "label1";
             this.label_status.Click += new System.EventHandler(this.label_status_Click);
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.dataToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1033, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(11, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1894, 38);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -85,42 +89,42 @@
             this.fitGMMsToolStripMenuItem,
             this.fitHGMsToolStripMenuItem});
             this.dataToolStripMenuItem.Name = "dataToolStripMenuItem";
-            this.dataToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.dataToolStripMenuItem.Size = new System.Drawing.Size(147, 32);
             this.dataToolStripMenuItem.Text = "Data Points";
             this.dataToolStripMenuItem.Click += new System.EventHandler(this.dataToolStripMenuItem_Click);
             // 
             // regenerateRandomDataToolStripMenuItem
             // 
             this.regenerateRandomDataToolStripMenuItem.Name = "regenerateRandomDataToolStripMenuItem";
-            this.regenerateRandomDataToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.regenerateRandomDataToolStripMenuItem.Size = new System.Drawing.Size(422, 40);
             this.regenerateRandomDataToolStripMenuItem.Text = "Generate Random Data";
             this.regenerateRandomDataToolStripMenuItem.Click += new System.EventHandler(this.regenerateRandomDataToolStripMenuItem_Click);
             // 
             // generateDummyGaussianDataToolStripMenuItem
             // 
             this.generateDummyGaussianDataToolStripMenuItem.Name = "generateDummyGaussianDataToolStripMenuItem";
-            this.generateDummyGaussianDataToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.generateDummyGaussianDataToolStripMenuItem.Size = new System.Drawing.Size(422, 40);
             this.generateDummyGaussianDataToolStripMenuItem.Text = "Generate Gaussian Data";
             this.generateDummyGaussianDataToolStripMenuItem.Click += new System.EventHandler(this.generateDummyGaussianDataToolStripMenuItem_Click_1);
             // 
             // drawDummyGaussianToolStripMenuItem
             // 
             this.drawDummyGaussianToolStripMenuItem.Name = "drawDummyGaussianToolStripMenuItem";
-            this.drawDummyGaussianToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.drawDummyGaussianToolStripMenuItem.Size = new System.Drawing.Size(422, 40);
             this.drawDummyGaussianToolStripMenuItem.Text = "Draw Gaussian Ground Truth";
             this.drawDummyGaussianToolStripMenuItem.Click += new System.EventHandler(this.drawDummyGaussianToolStripMenuItem_Click);
             // 
             // fitGMMsToolStripMenuItem
             // 
             this.fitGMMsToolStripMenuItem.Name = "fitGMMsToolStripMenuItem";
-            this.fitGMMsToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.fitGMMsToolStripMenuItem.Size = new System.Drawing.Size(422, 40);
             this.fitGMMsToolStripMenuItem.Text = "Fit Flat GMMs ";
             this.fitGMMsToolStripMenuItem.Click += new System.EventHandler(this.fitGMMsToolStripMenuItem_Click);
             // 
             // fitHGMsToolStripMenuItem
             // 
             this.fitHGMsToolStripMenuItem.Name = "fitHGMsToolStripMenuItem";
-            this.fitHGMsToolStripMenuItem.Size = new System.Drawing.Size(224, 22);
+            this.fitHGMsToolStripMenuItem.Size = new System.Drawing.Size(422, 40);
             this.fitHGMsToolStripMenuItem.Text = "Fit HGMMs";
             this.fitHGMsToolStripMenuItem.Click += new System.EventHandler(this.fitHGMsToolStripMenuItem_Click);
             // 
@@ -137,8 +141,10 @@
             this.groupbox_canvas.Controls.Add(this.generatedPoints);
             this.groupbox_canvas.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupbox_canvas.Location = new System.Drawing.Point(0, 0);
+            this.groupbox_canvas.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.groupbox_canvas.Name = "groupbox_canvas";
-            this.groupbox_canvas.Size = new System.Drawing.Size(1033, 549);
+            this.groupbox_canvas.Padding = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.groupbox_canvas.Size = new System.Drawing.Size(1894, 887);
             this.groupbox_canvas.TabIndex = 2;
             this.groupbox_canvas.TabStop = false;
             this.groupbox_canvas.Text = "Data Plot";
@@ -148,15 +154,17 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(949, 424);
+            this.label5.Location = new System.Drawing.Point(1740, 685);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.Size = new System.Drawing.Size(153, 21);
             this.label5.TabIndex = 9;
             this.label5.Text = "Configuration";
             // 
             // PointNumber
             // 
-            this.PointNumber.Location = new System.Drawing.Point(952, 452);
+            this.PointNumber.Location = new System.Drawing.Point(1745, 730);
+            this.PointNumber.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.PointNumber.Maximum = new decimal(new int[] {
             1000000000,
             0,
@@ -168,10 +176,10 @@
             0,
             0});
             this.PointNumber.Name = "PointNumber";
-            this.PointNumber.Size = new System.Drawing.Size(81, 20);
+            this.PointNumber.Size = new System.Drawing.Size(149, 31);
             this.PointNumber.TabIndex = 8;
             this.PointNumber.Value = new decimal(new int[] {
-            100,
+            1000,
             0,
             0,
             0});
@@ -180,9 +188,10 @@
             // 
             // LayerNumber
             // 
-            this.LayerNumber.Location = new System.Drawing.Point(952, 530);
+            this.LayerNumber.Location = new System.Drawing.Point(1745, 856);
+            this.LayerNumber.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.LayerNumber.Name = "LayerNumber";
-            this.LayerNumber.Size = new System.Drawing.Size(81, 20);
+            this.LayerNumber.Size = new System.Drawing.Size(149, 31);
             this.LayerNumber.TabIndex = 7;
             this.LayerNumber.Value = new decimal(new int[] {
             1,
@@ -194,9 +203,10 @@
             // 
             // SampleNumber
             // 
-            this.SampleNumber.Location = new System.Drawing.Point(952, 478);
+            this.SampleNumber.Location = new System.Drawing.Point(1745, 772);
+            this.SampleNumber.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.SampleNumber.Name = "SampleNumber";
-            this.SampleNumber.Size = new System.Drawing.Size(81, 20);
+            this.SampleNumber.Size = new System.Drawing.Size(149, 31);
             this.SampleNumber.TabIndex = 6;
             this.SampleNumber.Value = new decimal(new int[] {
             4,
@@ -208,9 +218,10 @@
             // 
             // FitNumber
             // 
-            this.FitNumber.Location = new System.Drawing.Point(952, 504);
+            this.FitNumber.Location = new System.Drawing.Point(1745, 814);
+            this.FitNumber.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.FitNumber.Name = "FitNumber";
-            this.FitNumber.Size = new System.Drawing.Size(81, 20);
+            this.FitNumber.Size = new System.Drawing.Size(149, 31);
             this.FitNumber.TabIndex = 5;
             this.FitNumber.Value = new decimal(new int[] {
             4,
@@ -223,9 +234,10 @@
             // HierarchicalLayers
             // 
             this.HierarchicalLayers.AutoSize = true;
-            this.HierarchicalLayers.Location = new System.Drawing.Point(858, 533);
+            this.HierarchicalLayers.Location = new System.Drawing.Point(1573, 861);
+            this.HierarchicalLayers.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.HierarchicalLayers.Name = "HierarchicalLayers";
-            this.HierarchicalLayers.Size = new System.Drawing.Size(88, 13);
+            this.HierarchicalLayers.Size = new System.Drawing.Size(197, 21);
             this.HierarchicalLayers.TabIndex = 3;
             this.HierarchicalLayers.Text = "Hierchical Layers";
             this.HierarchicalLayers.Click += new System.EventHandler(this.label4_Click);
@@ -233,45 +245,50 @@
             // GaussiansPerLevel
             // 
             this.GaussiansPerLevel.AutoSize = true;
-            this.GaussiansPerLevel.Location = new System.Drawing.Point(842, 506);
+            this.GaussiansPerLevel.Location = new System.Drawing.Point(1544, 817);
+            this.GaussiansPerLevel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.GaussiansPerLevel.Name = "GaussiansPerLevel";
-            this.GaussiansPerLevel.Size = new System.Drawing.Size(104, 13);
+            this.GaussiansPerLevel.Size = new System.Drawing.Size(219, 21);
             this.GaussiansPerLevel.TabIndex = 2;
             this.GaussiansPerLevel.Text = "Gaussians Per Level";
             // 
             // GaussianSamples
             // 
             this.GaussianSamples.AutoSize = true;
-            this.GaussianSamples.Location = new System.Drawing.Point(852, 480);
+            this.GaussianSamples.Location = new System.Drawing.Point(1562, 775);
+            this.GaussianSamples.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.GaussianSamples.Name = "GaussianSamples";
-            this.GaussianSamples.Size = new System.Drawing.Size(94, 13);
+            this.GaussianSamples.Size = new System.Drawing.Size(186, 21);
             this.GaussianSamples.TabIndex = 1;
             this.GaussianSamples.Text = "Gaussian Samples";
             // 
             // generatedPoints
             // 
             this.generatedPoints.AutoSize = true;
-            this.generatedPoints.Location = new System.Drawing.Point(854, 459);
+            this.generatedPoints.Location = new System.Drawing.Point(1566, 741);
+            this.generatedPoints.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.generatedPoints.Name = "generatedPoints";
-            this.generatedPoints.Size = new System.Drawing.Size(92, 13);
+            this.generatedPoints.Size = new System.Drawing.Size(197, 21);
             this.generatedPoints.TabIndex = 0;
             this.generatedPoints.Text = "Generated  Points";
             this.generatedPoints.Click += new System.EventHandler(this.label1_Click);
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // GMMDemoWnd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1033, 549);
+            this.ClientSize = new System.Drawing.Size(1894, 887);
             this.Controls.Add(this.label_status);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.groupbox_canvas);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "GMMDemoWnd";
             this.Text = "GMMDemo";
             this.menuStrip1.ResumeLayout(false);
--- GMMDemo/GMM.cs
+++ GMMDemo/GMM.cs
@@ -14,84 +14,6 @@ namespace GMMDemo
         public List<Vector2> pts = null;
         public List<Gaussian_2D> sample_gaussian_list = null;
 
-        public List<List<double>> EStep(List<Gaussian_2D> gaussian_list, List<double> class_prior)
-        {
-            int num_gaussians = gaussian_list.Count;
-
-            //pdfs: probability density function. 
-            //shape (row, column): (num_gaussian, num_point)
-            List<List<double>> pdfs = new List<List<double>>();
-
-            //calculate numerator: P(point=i | gaussian=j) * P(gaussian=j)
-            for (int j = 0; j < num_gaussians; ++j)
-            {
-                List<double> pdf = new List<double>();//init pdfs of gaussian j
-                for (int i = 0; i < pts.Count; ++i)
-                {
-                    pdf.Add(class_prior[j] * MatrixMath.MultivariateNormalPDF(pts[i], 
-                                                                            gaussian_list[j].miu, 
-                                                                            gaussian_list[j].Sigma));
-                }
-                pdfs.Add(pdf);
-            }
-
-            //TODO List.Sum()
-            for (int i = 0; i < pts.Count; ++i)
-            {
-                //calculate denominator: sum(pdfs, axis = 1)
-                double sum = 0;
-                for (int j = 0; j < num_gaussians; ++j)
-                {
-                    sum += pdfs[j][i];
-                }
-                //calculate W (inplace)
-                for (int j = 0; j < num_gaussians; ++j)
-                {
-                    pdfs[j][i] /= sum;
-                }
-            }
-
-            return pdfs;
-        }
-
-        public (List<Gaussian_2D> gaussian_list, List<double> class_prior) MStep(List<List<double>> W)
-        {
-            List<Gaussian_2D> gaussian_list = new List<Gaussian_2D>();
-            List<double> class_prior = new List<double>();
-
-            for (int j = 0; j < W.Count; j++)
-            {
-                double sum = 0;
-                Gaussian_2D gau = new Gaussian_2D();
-
-                //calculate class prior and mean
-                for (int i = 0; i < W[0].Count; i++)
-                {
-                    sum += W[j][i];//sum(W, axis=0)
-                    gau.miu.x += (float)W[j][i] * pts[i].x;//sum(Wi, xi)
-                    gau.miu.y += (float)W[j][i] * pts[i].y;//sum(Wi, yi)
-                }
-                gau.miu.x /= (float)sum;
-                gau.miu.y /= (float)sum;
-                class_prior.Add(sum / W[0].Count);
-
-                //calculate covariance
-                for (int i = 0; i < W[0].Count; i++)
-                {
-                    gau.Sigma.m00 += (float)(W[j][i] * Math.Pow(pts[i].x - gau.miu.x, 2));
-                    gau.Sigma.m10 = gau.Sigma.m01 += (float)W[j][i] * (pts[i].x - gau.miu.x) * (pts[i].y - gau.miu.y);
-                    gau.Sigma.m11 += (float)(W[j][i] * Math.Pow(pts[i].y - gau.miu.y, 2));
-                }
-                gau.Sigma.m00 /= (float)sum;
-                gau.Sigma.m10 = gau.Sigma.m01 /= (float)sum;
-                gau.Sigma.m11 /= (float)sum;
-
-                gaussian_list.Add(gau);
-            }
-
-            return (gaussian_list, class_prior);
-        }
-
         public List<Vector2> GenerateRandomPoints(int num_of_points, int xmax, int ymax) //x and y range from 0
         {
             pts = new List<Vector2>();
@@ -149,6 +71,88 @@ namespace GMMDemo
             return pts;
         }
 
+        public List<List<double>> EStep(List<Gaussian_2D> gaussian_list, List<double> class_prior)
+        {
+            int num_gaussians = gaussian_list.Count;
+
+            //pdfs: probability density function. 
+            //shape (row, column): (num_gaussian, num_point)
+            List<List<double>> pdfs = new List<List<double>>();
+
+            //calculate numerator: P(point=i | gaussian=j) * P(gaussian=j)
+            for (int j = 0; j < num_gaussians; ++j)
+            {
+                List<double> pdf = new List<double>();//init pdfs of gaussian j
+                double pdf_elm = new double();
+                for (int i = 0; i < pts.Count; ++i)
+                {
+                    pdf_elm = class_prior[j] * MatrixMath.MultivariateNormalPDF(pts[i],
+                                                                            gaussian_list[j].miu,
+                                                                            gaussian_list[j].Sigma);
+
+                    //if (pdf_elm == 0) pdf_elm = 1E-323;
+                    pdf.Add(pdf_elm);
+                }
+                pdfs.Add(pdf);
+            }
+
+            //TODO List.Sum()
+            for (int i = 0; i < pts.Count; ++i)
+            {
+                //calculate denominator: sum(pdfs, axis = 1)
+                double sum = 0;
+                for (int j = 0; j < num_gaussians; ++j)
+                {
+                    sum += pdfs[j][i];
+                }
+                //calculate W (inplace)
+                for (int j = 0; j < num_gaussians; ++j)
+                {
+                    pdfs[j][i] /= sum;
+                }
+            }
+
+            return pdfs;
+        }
+
+        public (List<Gaussian_2D> gaussian_list, List<double> class_prior) MStep(List<List<double>> W)
+        {
+            List<Gaussian_2D> gaussian_list = new List<Gaussian_2D>();
+            List<double> class_prior = new List<double>();
+
+            for (int j = 0; j < W.Count; j++)
+            {
+                double sum = 0;
+                Gaussian_2D gau = new Gaussian_2D();
+                //double gau.miu.x = Convert.ToDouble(gau.miu.x);
+                //calculate class prior and mean
+                for (int i = 0; i < W[0].Count; i++)
+                {
+                    sum += W[j][i];//sum(W, axis=0)
+                    gau.miu.x += (float)W[j][i] * (pts[i].x);//sum(Wi, xi)
+                    gau.miu.y += (float)W[j][i] * pts[i].y;//sum(Wi, yi)
+                }
+                gau.miu.x /= (float)sum;
+                gau.miu.y /= (float)sum;
+                class_prior.Add(sum / W[0].Count);
+
+                //calculate covariance
+                for (int i = 0; i < W[0].Count; i++)
+                {
+                    gau.Sigma.m00 += (float)(W[j][i] * Math.Pow(pts[i].x - gau.miu.x, 2));
+                    gau.Sigma.m10 = gau.Sigma.m01 += (float)W[j][i] * (pts[i].x - gau.miu.x) * (pts[i].y - gau.miu.y);
+                    gau.Sigma.m11 += (float)(W[j][i] * Math.Pow(pts[i].y - gau.miu.y, 2));
+                }
+                gau.Sigma.m00 /= (float)sum;
+                gau.Sigma.m10 = gau.Sigma.m01 /= (float)sum;
+                gau.Sigma.m11 /= (float)sum;
+
+                gaussian_list.Add(gau);
+            }
+
+            return (gaussian_list, class_prior);
+        }
+
         public List<Gaussian_2D> FitGaussians(int num_gaussians)
         {
             int max_iter = 10;
@@ -162,7 +166,7 @@ namespace GMMDemo
                 List<double> class_prior = new List<double>();
                 for (int i = 0; i < num_gaussians; i++)
                 {
-                    gaussian_list.Add(new Gaussian_2D(rand));
+                    gaussian_list.Add(new Gaussian_2D(rand, true));
                     class_prior.Add(1 / (double)(num_gaussians));
                 }
                 //init P(gaussian=j | point=i)
